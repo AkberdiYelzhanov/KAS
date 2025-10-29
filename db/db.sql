8c743a87f7be9261d05799d360722ee7915f6345
Akberdi Yelzhanov <ayelzhanov@gmail.com>
2025-10-29 11:19:34 +0500

    Changing DB file 2

--- db/db.sql
+++ db/db.sql
@@ -4,3 +4,7 @@ CREATE TABLE auth_db.users (
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+insert into auth_db.users (id, email, password_hash) values (1, 'test@test.com', 'test123');
+
+select * from auth_db.users;
